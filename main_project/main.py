c18269db5b11b4f36373c37d1b5956ce10494eab
Parth <parthwani22@gmail.com>
2023-04-21 00:44:10 +0530

    Modified main.py

--- main_project/main.py
+++ main_project/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_wtf import FlaskForm
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, EmailField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
+from werkzeug import security as s
 
 parking_system = Flask(__name__) # Application initialization.
 
@@ -40,8 +41,10 @@ class Vehicle(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     vehicle_status = db.Column(db.Boolean, nullable=False)
 
-with parking_system.app_context():
-    db.create_all()
+
+# # Creating database
+# with parking_system.app_context():
+#     db.create_all()
 
 # Forms
 class Loginform(FlaskForm):
@@ -60,10 +63,61 @@ class Signup(FlaskForm):
     signup_button = SubmitField('SIGNUP')
 
 
+
+with parking_system.app_context():
+    vehicle_list = Vehicle.query.all()  # Get all vehicle information
+    user_list = User.query.all()
+
+
+parking_system.config['SECRET_KEY'] = csrf_token
+
+
+
+# Function to hash passwords:
+def hash_password(password):
+    """This function hashes the user password. Converts password to SHA256 hash string with salt of length 8."""
+    hashed_password = s.generate_password_hash(password=password, method='pbkdf2:sha256', salt_length=8)
+    return hashed_password
+
+
+# Function to check passwords:
+def check_hash(user_hash, password):
+    """This function checks if the password entered by the user is correct by comparing hashes."""
+    check = s.check_password_hash(user_hash, password)
+    return check
+
+
 @parking_system.route("/")
 def home():
     return render_template('header.html')
 
 
+@parking_system.route("/login", methods=['GET', 'POST'])
+def login():
+    pass
+    # form = Loginform()
+    # if form.validate_on_submit():
+    #     for user in user_list:
+    #         if user.usr_email == form.email.data:
+    #             if check_hash(user_hash=user.password, form.password.data):
+    #                 login_user(user)
+    #                 return redirect(url_for('home'))
+    #             else:
+    #                 flash('Invalid Password')
+
+
+@parking_system.route("/signup", methods=['GET','POST'])
+def signup():
+    pass
+
+
+@parking_system.route("/logout")
+@login_required
+def logout():
+    pass
+
+
 if __name__ == '__main__':
-    parking_system.run(debug=True)
+    # print(vehicle_list[0].vehicle_status)
+    with parking_system.app_context():
+        parking_system.run(debug=True)
